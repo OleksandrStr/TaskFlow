0189ab0394d02ffccd046c263091cc3f91768e7b
OleksandrStr <92752633+OleksandrStr@users.noreply.github.com>
2024-02-26 19:30:55 +0100

    Add user registration on the server

---
+++ server/src/controllers/users.ts
@@ -0,0 +1,30 @@
+import {NextFunction, Request, Response} from 'express';
+import UserModel from '../models/user';
+import {UserDocument} from "../types/user.interface";
+import {Error} from "mongoose";
+
+const normalizeUser = (user: UserDocument) => {
+    return {
+        email: user.email,
+        username: user.username,
+        id: user.id
+    };
+};
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const newUser = new UserModel({
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password
+        });
+        const savedUser = await newUser.save();
+        res.send(normalizeUser(savedUser));
+    } catch (error) {
+        if (error instanceof Error.ValidationError) {
+            const messages = Object.values(error.errors).map(err => err.message);
+            return res.status(400).json(messages);
+        }
+        next(error);
+    }
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import {createServer} from 'http';
 import mongoose from "mongoose";
+import * as usersController from './controllers/users';
+import bodyParser from 'body-parser';
 
 const app = express();
 const httpServer = createServer(app);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.send('Server is UP');
 });
+
+app.post('/api/users', usersController.register);
 
 mongoose.connect('mongodb://localhost:27017/trello').then(() => {
     console.log('Connected to MongoDB');
