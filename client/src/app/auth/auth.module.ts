2ca2787409e3f32bd63de10239a9b4308bd052af
OleksandrStr <92752633+OleksandrStr@users.noreply.github.com>
2024-03-24 15:30:21 +0100

    Add registration component

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,7 +1,19 @@
 import {NgModule} from "@angular/core";
 import {AuthService} from "./services/auth.service";
+import {RegistrationComponent} from "./components/registration/registration.component";
+import {RouterModule, Routes} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  }
+];
 
 @NgModule({
-  providers: [AuthService]
+  imports: [RouterModule.forChild(routes), ReactiveFormsModule],
+  providers: [AuthService],
+  declarations: [RegistrationComponent]
 })
 export class AuthModule {}
---
+++ client/src/app/auth/components/registration/registration.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+import {FormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'auth-registration',
+  templateUrl: './registration.component.html'
+})
+export class RegistrationComponent {
+  form = this.fb.group({
+    email: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  })
+
+  onSubmit(): void {}
+
+  constructor(private fb: FormBuilder) {}
+}
