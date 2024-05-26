8ec108540bb31e9aa792ce2985fa8859ad6d895d
OleksandrStr <92752633+OleksandrStr@users.noreply.github.com>
2024-05-26 11:35:12 +0200

    Remove user pre-save password check

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -27,10 +27,6 @@ const userSchema = new Schema<UserDocument>(
 );
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-
   try {
     const salt = await bcryptjs.genSalt(10);
     this.password = await bcryptjs.hash(this.password, salt);
