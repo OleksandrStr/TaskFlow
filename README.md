e548852d0046e8be3926bee92675def099d42bba
OleksandrStr <92752633+OleksandrStr@users.noreply.github.com>
2024-08-18 10:22:50 +0200

    Change project name from trello to taskFlow

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Trello-Clone
+# TaskFlow
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,3 +1,3 @@
-export const mongodbUri = 'mongodb://localhost:27017/trello';
+export const mongodbUri = 'mongodb://localhost:27017/taskFlow';
 export const serverPort = 3000;
 export const jwtSecretKey = 'secretKey';
